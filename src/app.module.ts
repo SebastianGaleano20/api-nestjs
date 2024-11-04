6f38b1ca3e6514dbfd563b9e1c553d65e93b5712
SebastianGaleano20 <sebascarp9@gmail.com>
2024-11-04 12:45:38 -0300

    services created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { QuestionsModule } from './questions/questions.module';
+import { ResourcesModule } from './resources/resources.module';
+import { TechnologiesModule } from './technologies/technologies.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectsModule, QuestionsModule, ResourcesModule, TechnologiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
