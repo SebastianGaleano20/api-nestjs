3530a773818d306f6f2b1ea8a6429c448b97ddd9
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-05 10:04:41 -0300

    Decorator ROLES added

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -11,5 +11,5 @@ export class CreateProjectDto {
 
     @IsArray()
     @IsString()
-    technologies: string[]
+    technologies?: string[]
 }
--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {
+  @ApiProperty({ required: false })
+  name?: string;
+
+  @ApiProperty({ required: false })
+  description?: string;
+
+  @ApiProperty({ required: false })
+  technologies?: string[];
+}
