84819565acd86841e8d0f5518ad9e494a78f1c8c
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-05 09:34:49 -0300

    ProjectsService - create method added

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  //Creamos la instancia para interactuar con la db
+  constructor(private readonly prisma: PrismaService) { }
+  /* Metodo que crea un nuevo proyecto recibiendo por parametro la data
+     La estructura de la data debe respetar el CreateProjectDto */
+  create(createProjectDto: CreateProjectDto) { 
+    //Desestructuramos technologías y la data de los proyectos para trabajarlas por separado
+    const { technologies, ...projectData } = createProjectDto;
+    //Vamos a retornar como valor el resultado del metodo create de prisma:
+    return this.prisma.project.create({
+      data: {
+        //Le enviamos como data projectData desestructurada
+        ...projectData,
+        //Incluimos las technologías encontradas por el nombre recibido por parametro
+        technologies: {
+          connect: technologies.map(name => ({ name })),
+        },
+      },
+      //Incluimos la informacion de las technologías encontradas
+      include: {
+        technologies: true,
+      }
+    })
   }
 
   findAll() {
