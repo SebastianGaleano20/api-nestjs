afc7a41c1deef66c584b972c8d34ef0b71c93a21
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-16 10:18:35 -0300

    create-question DTO added

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,1 +1,14 @@
-export class CreateQuestionDto {}
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator'
+//Data Transfer Object de Questions
+export class CreateQuestionDto {
+    @IsString()
+    @IsNotEmpty()
+    question: string;
+    @IsString()
+    @IsNotEmpty()
+    answer: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    technologyId: number;
+}
