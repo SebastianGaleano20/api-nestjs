3ae6510130b9262a05bb46133a79ed1166ba39fb
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-16 10:39:29 -0300

    Method create question added

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
-import { UpdateQuestionDto } from './dto/update-question.dto';
+import { UpdateQuestionDto } from './dto/update-question-dto';
+import { FindQuestionsDto } from './dto/find-questions.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma, Question } from '@prisma/client';
 
 @Injectable()
 export class QuestionsService {
-  create(createQuestionDto: CreateQuestionDto) {
-    return 'This action adds a new question';
+  // Crea una instancia de Prisma para acceder a sus métodos
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(createQuestionDto: CreateQuestionDto): Promise<Question> {
+    // Verificar si existe la tecnología
+    const technology = await this.prisma.technology.findUnique({
+      where: { id: createQuestionDto.technologyId },
+    });
+
+    if (!technology) {
+      throw new NotFoundException(
+        `Technology with ID ${createQuestionDto.technologyId} not found`,
+      );
+    }
+
+    return this.prisma.question.create({
+      data: createQuestionDto,
+      include: {
+        technology: true,
+      },
+    });
   }
+
 
   findAll() {
     return `This action returns all questions`;
