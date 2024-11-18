acfd4510e1a09ba232dc3620028da31feb8309f9
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-18 10:36:21 -0300

    find-resources DTO added

---
+++ src/resources/dto/find-resource.dto.ts
@@ -0,0 +1,40 @@
+import { IsOptional, IsString, IsInt, IsEnum, Min } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+
+export class FindResourcesDto {
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @IsString()
+    search?: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    technologyId?: number;
+
+    @ApiProperty({ required: false, default: 0 })
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(0)
+    skip?: number;
+
+    @ApiProperty({ required: false, default: 10 })
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    take?: number;
+
+    @ApiProperty({ required: false, enum: ['title', ' createdAt'] })
+    @IsOptional()
+    @IsEnum(['title', 'createdAt'])
+    orderBy?: 'title' | 'createdAt';
+
+    @ApiProperty({ required: false, enum: ['asc', 'desc'] })
+    @IsOptional()
+    @IsEnum(['asc', 'desc'])
+    order?: 'asc' | 'desc';
+}
