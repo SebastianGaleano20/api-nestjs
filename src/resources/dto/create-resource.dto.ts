35c08fe8e3420e42377ee07d48a70f974b97cefe
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-18 10:22:43 -0300

    create-resource DTO added

--- src/resources/dto/create-resource.dto.ts
+++ src/resources/dto/create-resource.dto.ts
@@ -1,1 +1,15 @@
-export class CreateResourceDto {}
+import { IsString, IsNotEmpty, IsNumber, IsUrl } from "class-validator";
+export class CreateResourceDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsUrl()
+    url: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    technologyId: number;
+}
